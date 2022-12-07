0fd66c427eee9780ec20d4d387415f7641f789ad
Alina Costa <alinacosta87@gmail.com>
2022-12-07 21:58:42 +0200

    added products from sanity, added the banner and the footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import React from 'react';
+
+import { Layout } from '../components';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  // the component that we're currently on
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
