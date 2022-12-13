f57c2c845526268a561a5f4c2ed5fb21850f40a0
Alina Costa <alinacosta87@gmail.com>
2022-12-13 19:48:33 +0200

    added the Cart component

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Toaster } from 'react-hot-toast';
 
 import { Layout } from '../components';
 import '../styles/globals.css';
+import { StateContext } from '../context/StateContext';
 
 function MyApp({ Component, pageProps }) {
   // the component that we're currently on
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   );
 }
 
